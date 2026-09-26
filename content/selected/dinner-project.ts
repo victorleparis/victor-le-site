3bac101e59ed575754620d4bdcb2f37be485d998
Vik Le <v@shootme.com>
2026-09-26 18:27:23 +0200

    Correct Dinner Project guest count

--- content/selected/dinner-project.ts
+++ content/selected/dinner-project.ts
@@ -10,7 +10,7 @@ export const dinnerProject: SelectedProject = {
 };
 
 export const dinnerProjectPrinciple = [
-  "I invite 1–6 people for lunch or dinner.",
+  "I invite 2–4 people for lunch or dinner.",
   "Special guest can appear.",
   "I cook.",
   "They wear my clothes.",
